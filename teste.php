ace492c32a21b3852f22d8270f564aa5a2678a99
malobr <marcelotc1202@gmail.com>
2024-04-30 09:28:08 -0300

    Primeiro commit, Funcoes de calcular prontas

---
+++ teste.php
@@ -0,0 +1,7 @@
+<?php
+    $usuarios = [
+        ["usu" => "xorza", "nome" => "Gaybriel", "senha" => "meupintos123!#brian"]
+        ["usu" => "yezzzzzzz", "nome" => "Brian", "senha" => "amopeitospequenos"]
+        ["usu" => "Malo_the_gentleman", "nome" => "Marcelo", "senha" => "fazoL"]
+    ]
+?>
